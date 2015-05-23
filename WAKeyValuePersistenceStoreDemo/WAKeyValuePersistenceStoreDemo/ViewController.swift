083d2e3d8dd6841182d0af9319f7f7dd80c8004b
YuAo <me@imyuao.com>
2015-05-23 14:07:35 +0800

    update demo

--- WAKeyValuePersistenceStoreDemo/WAKeyValuePersistenceStoreDemo/ViewController.swift
+++ WAKeyValuePersistenceStoreDemo/WAKeyValuePersistenceStoreDemo/ViewController.swift
@@ -9,8 +9,24 @@
 import UIKit
 import WAKeyValuePersistenceStore
 
+private let ViewControllerTextCacheKey = "ViewControllerTextCacheKey"
+
 class ViewController: UIViewController {
+    
+    private let cacheStore = WAKeyValuePersistenceStore(directory: NSSearchPathDirectory.CachesDirectory, name: "CacheStore", objectSerializer: WAPersistenceObjectSerializer.keyedArchiveSerializer())
+    
+    @IBOutlet private weak var textField: UITextField!
+    @IBOutlet private weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.textField.text = self.cacheStore[ViewControllerTextCacheKey] as? String
+    }
 
-
+    @IBAction private func textFieldChanged(sender: UITextField) {
+        self.cacheStore[ViewControllerTextCacheKey] = sender.text
+        self.label.text = "Saved! See WAKeyValuePersistenceStoreDemoTests for more usage."
+    }
+    
 }
 
